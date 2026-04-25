1e8a56d6ecc38fe73f4cfeba4e4c1a04eada2f1c
Jorge Pauvels <jcvels@uvcoding.com.ar>
2026-04-25 16:46:58 -0300

    add: Libro class

---
+++ TP1-Colecciones/Libro.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TP1_Colecciones
+{
+    internal class Libro
+    {
+        public string titulo;
+        public string autor;
+        public string editorial;
+
+        public Libro(string titulo, string autor, string editorial)
+        {
+            this.titulo = titulo;
+            this.autor = autor;
+            this.editorial = editorial;
+        }
+        public string getTitulo()
+        {
+            return titulo;
+        }
+        public override string ToString()
+        {
+            return "Titulo: " + titulo + " Autor: " + autor + " Editorial: " + editorial;
+        }
+    }
+}
